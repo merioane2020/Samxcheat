c239f65c01954e1c203983f79fb9bd9e005430c7
merioane2020 <113840929+merioane2020@users.noreply.github.com>
2025-01-25 12:20:00 +0100

    Update Sam.js

--- Sam.js
+++ Sam.js
@@ -1,6 +1,6 @@
 function getUserCredentials() {
-    var correctUsername = "@mrwanxios"; // اسم المستخدم الصحيح
-    var correctPassword = "@mrwanxios"; // كلمة المرور الصحيحة
+    var correctUsername = "young"; // اسم المستخدم الصحيح
+    var correctPassword = "1"; // كلمة المرور الصحيحة
 
     while (true) {
         var username = prompt("𝘌𝘯𝘵𝘦𝘳 𝘜𝘴𝘦𝘳𝘯𝘢𝘮𝘦 :");
